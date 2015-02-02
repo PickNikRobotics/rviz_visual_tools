db436645e54aef90ac481ee8b5c21406c274f22e
andymcevoy <michael.mcevoy@colorado.edu>
2015-02-02 12:17:54 -0700

    added a clear overlay

--- src/rviz_visual_tools.cpp
+++ src/rviz_visual_tools.cpp
@@ -410,6 +410,12 @@ std_msgs::ColorRGBA RvizVisualTools::getColor(const rviz_visual_tools::colors &c
       result.g = 1.0;
       result.b = 0.2;
       break;
+    case CLEAR:
+      result.r=1.0;
+      result.g=1.0;
+      result.b=1.0;
+      result.a=0.0;
+      break;
     case PURPLE:
       result.r = 0.597;
       result.g = 0.0;
