0bb2c632de8c1a91e9b00c34a562fc40155631f1
Dave Coleman <davetcoleman@gmail.com>
2015-12-02 15:03:19 -0700

    Fixed TF publish rate Removed experimental code

--- src/tf_visual_tools.cpp
+++ src/tf_visual_tools.cpp
@@ -45,7 +45,7 @@ namespace rviz_visual_tools
 {
 TFVisualTools::TFVisualTools()
 {
-  double loop_hz = 15;  // hz
+  double loop_hz = 2;  // hz
   ros::Duration update_freq = ros::Duration(1.0 / loop_hz);
   non_realtime_loop_ = nh_.createTimer(update_freq, &TFVisualTools::publishAllTransforms, this);
 
