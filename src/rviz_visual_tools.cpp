585cfb31d41aac027661aa6e7cbfae5eb6ec5ced
Miguel Prada <miguel.prada.sarasola@gmail.com>
2016-08-05 21:53:41 +0200

    Change the sphere marker type from SPHERE_LIST to SPHERE. (#47)
    This makes irregularly scaled spheres (i.e. ellipsoids) to be rendered correctly.

--- src/rviz_visual_tools.cpp
+++ src/rviz_visual_tools.cpp
@@ -222,7 +222,7 @@ bool RvizVisualTools::loadRvizMarkers()
   // ID
   sphere_marker_.ns = "Sphere";
   // Set the marker type.
-  sphere_marker_.type = visualization_msgs::Marker::SPHERE_LIST;
+  sphere_marker_.type = visualization_msgs::Marker::SPHERE;
   // Set the marker action.  Options are ADD and DELETE
   sphere_marker_.action = visualization_msgs::Marker::ADD;
   // Marker group position and orientation
@@ -1022,15 +1022,10 @@ bool RvizVisualTools::publishSphere(const geometry_msgs::Pose &pose, const std_m
   else
     sphere_marker_.id = id;
 
+  sphere_marker_.pose = pose;
   sphere_marker_.color = color;
   sphere_marker_.scale = scale;
   sphere_marker_.ns = ns;
-
-  // Update the single point with new pose
-  sphere_marker_.points.resize(1); // TODO(davetcoleman): not sure why we need to resize it, but someone was growing this vector larger
-  sphere_marker_.colors.resize(1);
-  sphere_marker_.points[0] = pose.position;
-  sphere_marker_.colors[0] = color;
 
   // Helper for publishing rviz markers
   return publishMarker(sphere_marker_);
@@ -1047,13 +1042,10 @@ bool RvizVisualTools::publishSphere(const geometry_msgs::PoseStamped &pose, cons
   else
     sphere_marker_.id = id;
 
+  sphere_marker_.pose = pose.pose;
   sphere_marker_.color = getColor(color);
   sphere_marker_.scale = scale;
   sphere_marker_.ns = ns;
-
-  // Update the single point with new pose
-  sphere_marker_.points[0] = pose.pose.position;
-  sphere_marker_.colors[0] = getColor(color);
 
   // Helper for publishing rviz markers
   publishMarker(sphere_marker_);
