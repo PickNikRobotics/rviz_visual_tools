4655a5569b463c22ba5fa18ae48b5799ee69fc60
Dave Coleman <davetcoleman@gmail.com>
2017-08-12 22:58:21 -0600

    Update license year

--- src/tf_visual_tools.cpp
+++ src/tf_visual_tools.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2015, University of Colorado, Boulder
+ *  Copyright (c) 2017, PickNik Consulting
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
