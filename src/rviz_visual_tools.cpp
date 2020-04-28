9d9383d9f08eb41ef5202614c16b65be0ccb9a42
Bjar Ne <gleichdick@users.noreply.github.com>
2020-04-28 11:44:36 +0200

    add missing include of tf2_geometry_msgs

--- src/rviz_visual_tools.cpp
+++ src/rviz_visual_tools.cpp
@@ -40,6 +40,7 @@
 
 // Conversions
 #include <tf2_eigen/tf2_eigen.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2/convert.h>
 #include <tf2/LinearMath/Vector3.h>
 #include <tf2/LinearMath/Quaternion.h>
