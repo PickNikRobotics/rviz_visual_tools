4f23d60addbe6fc8b06e621155a1141308068374
Bjar Ne <gleichdick@users.noreply.github.com>
2020-06-02 22:19:01 +0200

    remove unneeded includes

--- src/rviz_visual_tools.cpp
+++ src/rviz_visual_tools.cpp
@@ -42,8 +42,6 @@
 #include <tf2_eigen/tf2_eigen.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2/convert.h>
-#include <tf2/LinearMath/Vector3.h>
-#include <tf2/LinearMath/Quaternion.h>
 
 // C++
 #include <cmath>  // for random poses
